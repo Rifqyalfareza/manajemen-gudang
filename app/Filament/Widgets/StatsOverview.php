57a0c842c984aaa7051f1b532c4decaf4c722f0c
Rifqyalfareza <rifqyalfareza30@gmail.com>
2025-08-01 16:27:28 +0700

    remove chart color on the stats overview

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -21,34 +21,28 @@ class StatsOverview extends BaseWidget
                 ->count())
                 ->description('Jumlah total produk yang tersedia.')
                 ->descriptionColor('primary')
-                ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('primary'),
             Stat::make('Total Supplier', Supplier::count())
                 ->description('Jumlah total supplier terdaftar.')
                 ->descriptionColor('primary')
-                ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('primary'),
             Stat::make('Total Pelanggan', Customer::count())
                 ->description('Jumlah total pelanggan terdaftar.')
                 ->descriptionColor('primary')
-                ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('primary'),
             Stat::make('Total Stok Masuk', StockIn::sum('quantity'))
                 ->description('Jumlah total stok masuk dari semua produk.')
                 ->descriptionColor('success')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('success'),
             Stat::make('Total Stok Keluar', StockOut::sum('quantity'))
                 ->description('Jumlah total stok keluar dari semua produk.')
                 ->descriptionColor('danger')
                 ->descriptionIcon('heroicon-m-arrow-trending-down')
-                ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('danger'),
             Stat::make('Barang Masuk', StockIn::whereDate('date', today())->count())
                 ->description('Jumlah barang masuk hari ini.')
                 ->descriptionColor('warning')
-                ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('warning'),
         ];
     }
